277f588ce21df36d9deaf568015ea7ab7a240b1b
dragonwarrior61 <amodfired@gmail.com>
2025-04-29 04:02:28 +0300

    add config file

---
+++ config.py
@@ -0,0 +1,7 @@
+CLIENT_SECRETS_FILE = 'client_secrets.json'
+MERCHANT_ID = 'YOUR_CSS_MERCHANT_ID'  # Found in your Merchant Center account
+
+# API constants
+API_SCOPE = 'https://www.googleapis.com/auth/content'
+API_VERSION = 'v2.1'
+DISCOVERY_URL = 'https://shoppingcontent.googleapis.com/discovery/v1/apis/content/{}/rest'.format(API_VERSION)
