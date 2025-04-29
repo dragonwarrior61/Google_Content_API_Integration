d47d8c8f91b475cec3402c34e107058e453cb40a
dragonwarrior61 <amodfired@gmail.com>
2025-04-29 05:15:26 +0300

    update main code

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from shopping_api import ShoppingContentAPI
+from data_processing import parse_xml
+from time import time
 
-def main():
+def main(url):
     api = ShoppingContentAPI()
     
-    result = api.insert_product(product)
+    products = parse_xml(url)
+    for product in products:
+        result = api.insert_product(product)
+        time.asleep(0.1)
     print(result)
     
     
 if __name__ == "__main__":
-    main()
+    url = "https://wasje.nl/wp-content/uploads/woo-feed/google/xml/shopping.xml"
+    main(url)
