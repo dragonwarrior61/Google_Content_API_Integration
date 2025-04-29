26579b019475912dfc5e14f198da47e2f364bb83
dragonwarrior61 <amodfired@gmail.com>
2025-04-29 04:08:32 +0300

    add auth file

--- auth.py
+++ auth.py
@@ -2,6 +2,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 import os
+import config
 
 def authenticate():
     creds = None
