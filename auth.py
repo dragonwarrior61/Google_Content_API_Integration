b75da3edae10278d7631ae82bdace47462eb4020
dragonwarrior61 <amodfired@gmail.com>
2025-05-02 12:27:41 +0300

    change some parameter name

--- auth.py
+++ auth.py
@@ -9,11 +9,11 @@ import json
 def authenticate():
     creds = None
     token_file = 'token.json'
-    client_secrets_file = 'client.json'
+    client_file = 'client.json'
     
-    if os.path.exists(client_secrets_file):
-        with open(client_secrets_file, 'r') as f:
-            client_secrets = json.load(f)["web"]
+    if os.path.exists(client_file):
+        with open(client_file, 'r') as f:
+            client_data = json.load(f)["web"]
 
     if os.path.exists(token_file):
         with open(token_file, 'r') as f:
@@ -22,8 +22,8 @@ def authenticate():
             token=token_data.get("access_token"),
             refresh_token=token_data.get("refresh_token"),
             token_uri='https://oauth2.googleapis.com/token',
-            client_id=client_secrets["client_id"],
-            client_secret=client_secrets["client_secret"],
+            client_id=client_data["client_id"],
+            client_secret=client_data["client_secret"],
             scopes=['https://www.googleapis.com/auth/content']
         )
 
